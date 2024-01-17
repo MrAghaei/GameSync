1989b90c26638a18710b7285f26a10c4bf6f802d
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-01-17 16:23:44 +0330

    [#9-ADD] SwiperItemContainer component

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,10 +1,12 @@
 import Item from "../components/Item";
+import SwiperItemContainer from "../components/SwiperItemContainer";
 
 function LandingPage() {
+  const landingPageHeadline = ["Popular", "Top Games"];
   return (
     <div className={"bg-amber-300 container mx-auto"}>
-      Landing Page
-      <Item />
+      <SwiperItemContainer headline={landingPageHeadline[0]} />
+      <SwiperItemContainer headline={landingPageHeadline[1]} />
     </div>
   );
 }
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -7,49 +7,41 @@ function Item() {
   let gameName = "Metro Exodus 2023";
   let metacriticScore = 88;
   let platforms = ["playstation", "xbox"];
-  const buttoneType = "Add";
+  const buttonType = "Add";
+
   function handleButtonClick() {
     console.log("button clicked");
   }
+
   return (
-    <div
-      className={
-        "bg-black-secondary w-80 h-96 rounded-2xl flex flex-col overflow-hidden"
-      }
-    >
-      <div className={"w-full h-1/2  "}>
+    <div className="bg-black-secondary max-w-full md:max-w-72 h-auto md:h-96 rounded-2xl flex flex-col overflow-hidden">
+      <div className="w-full h-1/2">
         <img
           alt={imageAlt}
           src={imageSource}
-          className={"object-cover w-full h-full"}
+          className="object-cover w-full h-full"
         />
       </div>
 
-      <div
-        className={
-          "bg-black-secondary  w-full h-44 p-3 flex flex-col gap-4 p-5"
-        }
-      >
-        <div className={"flex gap-36 items-center"}>
+      <div className="bg-black-secondary w-full h-auto md:h-44 p-3 flex flex-col gap-4 p-5">
+        <div className="flex flex-col sm:flex-row gap-4 md:items-center">
           <p
             className={`border-2 inline p-1 w-max rounded ${
               metacriticScore >= 75
                 ? "text-green"
                 : metacriticScore >= 50
                   ? "text-yellow"
-                  : metacriticScore < 50
-                    ? "text-red "
-                    : ""
+                  : "text-red"
             }`}
           >
             {metacriticScore}
           </p>
           <PlatformImages platforms={platforms} />
         </div>
-        <h3 className={"font-bold text-2xl text-white"}>{gameName}</h3>
+        <h3 className="font-bold text-2xl text-white">{gameName}</h3>
       </div>
 
-      <Button buttonType={buttoneType} handleButtonClick={handleButtonClick} />
+      <Button buttonType={buttonType} handleButtonClick={handleButtonClick} />
     </div>
   );
 }
