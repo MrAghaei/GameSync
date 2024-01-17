ff3e8b110af938f23bc1c1c3d0c3e9b8b9abb070
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-01-17 09:37:37 +0330

    [#10-UPDATE] item component
    edit tailwind colors

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,10 +2,11 @@ function Item() {
   let imageAlt;
   let imageSource = "./testback.jpg";
   let gameName = "Metro Exodus 2023";
+  let metacriticScore = 88;
   return (
     <div
       className={
-        "bg-slate-800 size-96 rounded-md flex flex-col overflow-hidden"
+        "bg-slate-800 w-80 h-96 rounded-md flex flex-col overflow-hidden"
       }
     >
       <div className={"w-full h-1/2  "}>
@@ -16,8 +17,26 @@ function Item() {
         />
       </div>
 
-      <div className={"bg-slate-700 text-white "}>
-        <h3>{gameName}</h3>
+      <div
+        className={"bg-black-secondary  w-full h-full p-3 flex flex-col gap-4"}
+      >
+        <div className={"flex gap-36"}>
+          <p
+            className={`border-2 inline p-1 w-max rounded ${
+              metacriticScore >= 75
+                ? "text-green"
+                : metacriticScore >= 50
+                  ? "text-yellow"
+                  : metacriticScore < 50
+                    ? "text-red "
+                    : ""
+            }`}
+          >
+            {metacriticScore}
+          </p>
+          <p className={""}>logo</p>
+        </div>
+        <h3 className={"font-bold text-2xl text-text-white"}>{gameName}</h3>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "black-secondary": "#232323",
+      "text-white": "#faf5ff",
+      green: "#16a34a",
+      yellow: "#eab308",
+      red: "#dc2626",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
