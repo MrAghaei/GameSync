166197e1cc987a53e7565a1a37e36074dbaa78c6
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-01-20 16:30:03 +0330

    [#13-UPDATE] data interface

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,27 +1,24 @@
 import TextField from "@mui/material/TextField";
 import { InputAdornment } from "@mui/material";
-
 import SearchIcon from "@mui/icons-material/Search";
-function SearchBar({ handleSearch }) {
+
+interface SearchBarInputDataModel {
+  handleSearch: any;
+}
+export interface SearchBarInputModel {
+  data: SearchBarInputDataModel;
+}
+
+function SearchBar(data: SearchBarInputModel) {
+  console.log(data);
+  console.log(data.data);
   return (
     <div className={"bg-gray-dark w-full h-16 p-8 "}>
-      {/*<div className={"relative"}>*/}
-      {/*  <input*/}
-      {/*    type={"text"}*/}
-      {/*    placeholder={"Search..."}*/}
-      {/*    className={*/}
-      {/*      "bg-black-secondary p-3 border border-gray-300 rounded-xl focus:outline-none w-2/3"*/}
-      {/*    }*/}
-      {/*  />*/}
-      {/*  <div className="absolute left-0 top-0 bottom-0 flex items-center px-2 size-10">*/}
-      {/*    <img src={"./search-icon.svg"} className={"size-10"} />*/}
-      {/*  </div>*/}
-      {/*</div>*/}
       <form>
         <TextField
           id="search-bar"
           className="bg-black-secondary rounded-3xl w-1/2"
-          onChange={(e) => handleSearch(e.target.value)}
+          onChange={(e) => data.data.handleSearch(e.target.value)}
           label=""
           InputProps={{
             startAdornment: (
