98ba16d9f9be4f1be67aeb18a0dfb652b67acec1
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-01-20 10:24:30 +0330

    #11 implement nav bar component (#8)
    * [#11-ADD] navbar component
    
    * [#11-UPDATE] navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
-    <>
+    <div className={"flex flex-row"}>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path={"searchpage"} element={<SearchPage />}></Route>
           <Route path={"toplaypage"} element={<ToPlayPage />}></Route>
           <Route path={"playingpage"} element={<PlayingPage />}></Route>
           <Route path={"playedpage"} element={<PlayedPage />}></Route>
-          <Route path={"landing"} element={<LandingPage />}></Route>
+          <Route path={"landingpage"} element={<LandingPage />}></Route>
           <Route path={"*"} element={<NotFoundPage />}></Route>
         </Routes>
       </BrowserRouter>
-      {/*<NavBar />*/}
-    </>
+    </div>
   );
 }
 
