a6bea3bfc611f0a5d3a0963c06331f6b366365f2
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-01-22 13:07:28 +0330

    [#20-ADD] active functionality to navlinks

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,24 +9,69 @@ function NavBar() {
     >
       <img src={"./logo.png"} />
 
-      <NavLink to={"/landingpage"} className={"flex gap-2 items-center"}>
-        <img src={"./home-icon.svg"} className={"size-6"} />
+      <NavLink
+        to={"/landingpage"}
+        className={({ isActive, isTransitioning }) =>
+          [
+            "flex gap-2 items-center ",
+            isActive ? "text-purple" : "",
+            isTransitioning ? "text-white" : "",
+          ].join(" ")
+        }
+      >
+        <img src={"./home-icon.svg"} className={"size-6 "} />
         <p>Home</p>
       </NavLink>
-      <NavLink to={"/searchpage"} className={"flex gap-2 items-center"}>
+      <NavLink
+        to={"/searchpage"}
+        className={({ isActive, isTransitioning }) =>
+          [
+            "flex gap-2 items-center ",
+            isActive ? "text-purple" : "",
+            isTransitioning ? "text-white" : "",
+          ].join(" ")
+        }
+      >
         <img src={"./search-icon.svg"} className={"size-6"} />
         <p>Search</p>
       </NavLink>
 
-      <NavLink to={"/toplaypage"} className={"flex gap-2 items-center"}>
+      <NavLink
+        to={"/toplaypage"}
+        className={({ isActive, isTransitioning }) =>
+          [
+            "flex gap-2 items-center ",
+            isActive ? "text-purple" : "",
+            isTransitioning ? "text-white" : "",
+          ].join(" ")
+        }
+      >
         <img src={"./toplay-icon.svg"} className={"size-6"} />
         <p>To Play</p>
       </NavLink>
-      <NavLink to={"/playingpage"} className={"flex gap-2 items-center"}>
+      <NavLink
+        to={"/playingpage"}
+        className={({ isActive, isTransitioning }) =>
+          [
+            "flex gap-2 items-center ",
+            isActive ? "text-purple" : "",
+            isTransitioning ? "text-white" : "",
+          ].join(" ")
+        }
+      >
         <img src={"./playing-icon.svg"} className={"size-6"} />
         <p>Playing</p>
       </NavLink>
-      <NavLink to={"/playedpage"} className={"flex gap-2 items-center"}>
+      <NavLink
+        to={"/playedpage"}
+        className={({ isActive, isTransitioning }) =>
+          [
+            "flex gap-2 items-center ",
+            isActive ? "text-purple" : "",
+            isTransitioning ? "text-white" : "",
+          ].join(" ")
+        }
+      >
         <img src={"./played-icon.svg"} className={"size-6"} />
         <p>Played</p>
       </NavLink>
