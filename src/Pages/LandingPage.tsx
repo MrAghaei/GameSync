34707566d8d4907ae85500815c69b6516eabd244
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-02-01 11:05:38 +0330

    [#25-FIX] landing page adding (#27)

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -17,16 +17,16 @@ function LandingPage() {
   const [open, setOpen] = useState(false);
   const [currentGameId, setCurrentGameId] = useState("");
   const [dialogValue, setDialogValue] = useState("");
-  const [gameIds, setGameIds] = useState(() =>
-    getGameIdsFromStorage("toplaypage"),
-  );
+  // const [gameIds, setGameIds] = useState(() =>
+  //   getGameIdsFromStorage("toplaypage"),
+  // );
   useEffect(() => {
     const fetchdata = async () => {
       const items = await fetchLandingPageGames();
       setGameItems(items);
     };
     fetchdata();
-  }, [gameIds]);
+  }, []);
   const handleOpenDialog = (gameId: string) => {
     setCurrentGameId(gameId);
     setOpen(true);
@@ -36,7 +36,7 @@ function LandingPage() {
     if (!value) return;
     setDialogValue(value);
     transferItemFromLandingPage(currentGameId, value);
-    setGameIds(getGameIdsFromStorage("landingpage"));
+    // setGameIds(getGameIdsFromStorage("landingpage"));
   };
   return (
     <div className={"bg-gray-dark p-10 flex flex-col gap-10"}>
